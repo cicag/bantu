3d1f5c58ab8c61e011d86fe61cb01e0cbd4fdcf4
cicag <riiiooooo13@gmail.com>
2025-04-30 17:01:58 +0700

    Update index.php to match current

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <?php
 require 'functions.php';
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BANTU</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <h1>BANTU</h1>
-    <form action="index2.php" method="POST">
-        <input type="text" name="keyword" size="60" autofocus placeholder="pusing, gemetar" autocomplete="off">
-        <button type="submit" name="cari"></a>Cari!</button>
-    </form>
+<body class="page">
+
+<div class="center-container">
+        <h1 class="title">BANTU</h1>
+
+        <form action="index2.php" method="POST" class="search-form">
+            <input type="text" name="keyword" class="search-input" size="60" autofocus placeholder="pusing, gemetar" autocomplete="off">
+            <button type="submit" name="cari" class="search-button">Cari!</button>
+        </form>
+    </div>
 </body>
+
 </html>
