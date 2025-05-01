baf2665f1457750b54359f9d300ba04fb083f5d8
cicag <riiiooooo13@gmail.com>
2025-05-01 09:03:34 +0700

    menambah css index1.php

--- index.php
+++ index.php
@@ -4,14 +4,28 @@ require 'functions.php';
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.25">
     <title>BANTU</title>
+    <link href="https://fonts.googleapis.com/css2?family=Hammersmith+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <h1>bantu.</h1>
-    <h2>Bagaimana keadaan anda?</h2>
-    <form action="index2.php" method="POST">
-        <input type="text" name="keyword" size="60" autofocus placeholder="pusing, gemetar" autocomplete="off">
-        <button type="submit" name="cari"></a>Cari!</button>
+<body class="page">
+<div class="wadahtengah">
+    <h1 class="title">bantu.</h1>
+    <h2 class="subtitle">Bagaimana keadaan anda?</h2>
+    <form action="index2.php" method="POST" class="formsearch">
+        <input type="text" name="keyword" class="input" size="50" autofocus placeholder="  pusing, gemetar" autocomplete="off">
+        <button type="submit" name="cari" class="button">
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="#5f6368">
+                <path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+    </svg>
+        </button>
     </form>
+</div>
+<footer class="footer">
+    bantu v0.0.1-pre-alpha
+</footer>
+
 </body>
 </html>
